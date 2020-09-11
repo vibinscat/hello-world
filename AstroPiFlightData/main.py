8a882d49efe63a1ef251af28c205db94c52a4cef
Vibin Nadakkal <vibinscat@hotmail.com>
2020-09-11 15:19:00 +1000

    Added new data

--- AstroPiFlightData/main.py
+++ AstroPiFlightData/main.py
@@ -7,7 +7,23 @@ Created on Fri Sep  4 13:21:17 2020
 """
 
 import pandas as pd
+import numpy as np
+from datetime import datetime
+
 
 df  = pd.read_csv('Columbus_Ed_astro_pi_datalog.csv')
 
+list_timestamp = df['time_stamp']
 
+new_col = []
+
+for items in list_timestamp:
+    new_col.append(datetime.strptime(items[0:10],"%Y-%m-%d"))
+
+print(new_col)
+
+
+date_col = pd.Series(new_col)
+
+df['new_col'] = date_col
+# 
