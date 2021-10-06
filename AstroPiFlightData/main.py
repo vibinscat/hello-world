ce676ea6ed0d702292c8b25fb1f060ad9faf3375
Vibin Nadakkal <vibinscat@hotmail.com>
2021-10-06 14:16:59 +1100

    Update to code

--- AstroPiFlightData/main.py
+++ AstroPiFlightData/main.py
@@ -5,24 +5,28 @@ Created on Fri Sep  4 13:21:17 2020
 
 @author: vibinscat
 """
-
+%matplotlib inline
 import pandas as pd
 import numpy as np
 from datetime import datetime
 
 
-df  = pd.read_csv('Columbus_Ed_astro_pi_datalog.csv')
+df  = pd.read_csv('Columbus_Ed_astro_pi_datalog.csv', index_col=('ROW_ID'))
+df2 = pd.read_csv('Columbus2_Ed_astro_pi_datalog.csv', index_col=('ROW_ID'))
 
-list_timestamp = df['time_stamp']
+columbus_data = pd.concat([df,df2])
+
+list_timestamp = columbus_data['time_stamp']
 
 new_col = []
 
 for items in list_timestamp:
     new_col.append(datetime.strptime(items[0:10],"%Y-%m-%d"))
 
-print(new_col)
 
 
 date_col = pd.Series(new_col)
 
-df['new_col'] = date_col
+columbus_data['Date'] = date_col
+
+columbus_data.columns
